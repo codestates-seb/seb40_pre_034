880e175657dcf41c6c31c1a9bdb058765d198fe0
oasis791 <khwi791@naver.com>
2022-10-25 16:05:43 +0900

    Initial Commit

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/StackoverflowCloneApplication.java
@@ -0,0 +1,13 @@
+package seb40pre034.stackoverflowclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackoverflowCloneApplication.class, args);
+	}
+
+}
