e831d58cc68bccb80adcc52e70a31de16b2ed7b8
ClimberCode <kcjckswnd@gamil.com>
2022-10-26 22:45:55 +0900

    :spakles: feat : MemberDto 구현

--- server/src/main/java/seb40pre034/stackoverflowclone/member/dto/MemberDto.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/member/dto/MemberDto.java
@@ -1,2 +1,56 @@
-package seb40pre034.stackoverflowclone.member.dto;public class MemberDto {
+package seb40pre034.stackoverflowclone.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import seb40pre034.stackoverflowclone.member.entity.Member;
+
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+public class MemberDto {
+
+    @Getter
+    @AllArgsConstructor
+    public static class Post {
+        @NotBlank
+        @Email
+        private String email;
+
+        @NotBlank(message = "닉네임을 입력해 주세요ㅕ")
+        private String nickName;
+
+        @NotBlank(message = "패스워드를 입력해 주세요.")
+        private String password;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Patch{
+        private Long memberId;
+
+        @NotBlank
+        @Email
+        private String email;
+
+        @NotBlank(message = "닉네임을 입력해 주세요ㅕ")
+        private String nickName;
+
+        @NotBlank(message = "패스워드를 입력해 주세요.")
+        private String password;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class Response{
+        private Long memberId;
+        private String email;
+        private String nickName;
+        private String password;
+        private Member.MemberStatus status;
+
+        public String getMemberStatus(){
+            return status.getStatus();
+        }
+    }
 }
