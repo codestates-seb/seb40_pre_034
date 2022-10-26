584477c495f9049ed4f3f7d2c9fc87bc6efc3960
ClimberCode <kcjckswnd@gamil.com>
2022-10-26 11:37:21 +0900

    :sparkles: feat: member Entity, Repository 생성

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package seb40pre034.stackoverflowclone.member.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository {
+
+}
