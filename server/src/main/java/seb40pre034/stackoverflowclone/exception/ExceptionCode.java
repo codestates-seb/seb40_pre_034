e0919661d074bae9438925da6cbeca2e74a6f50a
oasis791 <khwi791@naver.com>
2022-10-26 14:23:02 +0900

    :sparkles: feat: Question Service 구현

--- server/src/main/java/seb40pre034/stackoverflowclone/exception/ExceptionCode.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/exception/ExceptionCode.java
@@ -5,7 +5,8 @@ import lombok.Getter;
 public enum ExceptionCode {
     ANSWER_NOT_FOUND(404, "Answer not found"),
     MEMBER_NOT_FOUND(404, "Member not found"),
-    MEMBER_EXISTS(409, "Member exists");
+    MEMBER_EXISTS(409, "Member exists"),
+    QUESTION_NOT_FOUND(404, "Question not found");
 
 
     @Getter
---
+++ server/src/main/java/seb40pre034/stackoverflowclone/question/service/QuestionService.java
@@ -0,0 +1,20 @@
+package seb40pre034.stackoverflowclone.question.service;
+
+import seb40pre034.stackoverflowclone.question.entity.Question;
+
+import java.util.List;
+
+public interface QuestionService {
+    //CRUD
+    public Question createQuestion(Question question);
+
+    public Question updateQuestion(Question question);
+
+    public Question findQuestion(long questionId);
+
+    public List<Question> findQuestions();
+
+    public void deleteQuestion(long questionId);
+
+    public Question findVerifiedQuestion(long questionId);
+}
