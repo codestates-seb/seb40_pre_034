2dae7d10fa06f1c1295c64434ccccc7d72bebeb9
oasis791 <khwi791@naver.com>
2022-10-26 11:31:41 +0900

    :sparkles: feat: Question Entity 생성

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/question/dto/Question.java
@@ -0,0 +1,34 @@
+package seb40pre034.stackoverflowclone.question.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+import seb40pre034.stackoverflowclone.audit.Auditable;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "QUESTION")
+@Entity
+public class Question extends Auditable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long questionId;
+
+    @Column(length = 25, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    @ColumnDefault("0")
+    private Integer vote;
+
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    private Long memberId;
+}
