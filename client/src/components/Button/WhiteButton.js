627cd6d0c30ba980c10aae09e2eab9ff6c6403fc
EUNSEO814 <eunseo814@gmail.com>
2022-10-26 13:48:08 +0900

    💄 design :  버튼 컴포넌트 중 WhiteButton 완성

---
+++ client/src/components/Button/WhiteButton.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const WhiteButton = styled.button`
+  width: ${(props) => (props.width ? props.width : "auto")};
+  color: hsl(206, 100%, 40%);
+  border: calc(1px / 2) solid hsl(206 85% 57%);
+  background-color: hsl(0, 0%, 100%);
+  &:hover {
+    background-color: #f0f8ff;
+  }
+`;
+
+{
+  /* * width를 props로 내려주거나, 굳이 필요없으면 글씨 길이만큼 늘어납니다.
+  ex1)
+  <WhiteButton>join</WhiteButton>
+<WhiteButton width="150px">join</WhiteButton> */
+}
