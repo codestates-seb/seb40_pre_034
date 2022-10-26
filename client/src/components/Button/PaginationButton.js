a0749b6148a85c9b0e4e1297e0c5928a4010e851
EUNSEO814 <eunseo814@gmail.com>
2022-10-26 13:32:38 +0900

    💡 comment : 버튼 컴포넌트 중 PaginationButton 사용예시 주석추가

--- client/src/components/Button/PaginationButton.js
+++ client/src/components/Button/PaginationButton.js
@@ -13,10 +13,14 @@ export const PaginationButton = styled.button`
   }
 `;
 {
-  /* * width를 props로 내려주거나, 굳이 필요없으면 글씨 길이만큼 늘어납니다.
-  ex1)
-      <PaginationButton>1</PaginationButton>
-      <PaginationButton checked>1</PaginationButton>
-      <PaginationButton>Prev</PaginationButton>
-      <PaginationButton checked>next</PaginationButton> */
+  /* * 1. width를 props로 내려주거나, 굳이 필요없으면 글씨 길이만큼 늘어납니다.
+    2. 컴포넌트에 checked를 같이 넣어주면 선택된 버튼은 주황색으로 나옵니다.
+    나중에 기능 구현하실 때 조건부 렌더링에 이용해보실 수 도 있을 것 같습니다.
+    만약 잘 안되면 알려주세요. 두 버전으로 나누겠습니다.
+    ex1)
+    <PaginationButton>1</PaginationButton>
+    <PaginationButton checked>1</PaginationButton>
+    ex2)
+    <PaginationButton>Prev</PaginationButton>
+    <PaginationButton checked>next</PaginationButton> */
 }
