465c2bd72d2d6d8a8f2923237b0745868a577ada
EUNSEO814 <eunseo814@gmail.com>
2022-10-28 00:21:53 +0900

    💄 design : Aside Component 구현

---
+++ client/src/components/Aside/YellowCard/YCExample.js
@@ -0,0 +1,56 @@
+// 1. import 잘해주세요! 밑에있는거 다 필요합니다!(아이콘은 필요한 아이콘만 import하셔도 됩니다.)
+
+import { CardStyle } from "../CardStyle";
+import { YellowCardStyles, Pencil, Square, StackOverFlow } from "./YellowCardStyles";
+import YellowCardTitle from "./YellowCardTitle";
+import YellowCardContent from "./YellowCardContent";
+import styled from "styled-components";
+
+// 2. 아이콘이랑 글자 가운데 정렬이 너무 하고 싶어서 추가했습니다 ㅎㅎ,, 밑에 코드 그대로 가져가셔서 사용하시면 됩니다.
+const InnerContent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+// 3. 모든 경우의 수를 예시로 다 넣어 뒀습니다. 너무 많아서 이해가 잘 안되시면 말씀해주세요! 그냥 이부분은 제가 하겠습니다...(컴포넌트 재사용 쉽지않네요..ㅠㅠ)
+const YCExample = () => {
+  return (
+    <>
+      {/* 4. CardStyle, YellowCardStyles로 꼭 감싸주세요! */}
+      <CardStyle>
+        <YellowCardStyles>
+          {/* 5. 순서: YellowCardTitle -> InnerContent ->(아이콘컴포넌트, YellowCardContent) */}
+          <YellowCardTitle text="The Overflow Blog" />
+          <InnerContent>
+            <Pencil />
+            <YellowCardContent text="Goodbye Webpack, hello Turbopack! The big news from today’s Next.JS conference" />
+          </InnerContent>
+          <InnerContent>
+            <Pencil />
+            <YellowCardContent text="CEO update: Breaking down barriers to unlock innovation" />
+          </InnerContent>
+
+          <YellowCardTitle text="Featured on Meta" />
+          <InnerContent>
+            <Square />
+            <YellowCardContent text="Goodbye Webpack, hello Turbopack! The big news from today’s Next.JS conference" />
+          </InnerContent>
+          <InnerContent>
+            <Square />
+            <YellowCardContent text="Mobile app infrastructure being decommissioned" />
+          </InnerContent>
+          <InnerContent>
+            <StackOverFlow />
+            <YellowCardContent text="Staging Ground Workflow: Canned Comments" />
+          </InnerContent>
+          <InnerContent>
+            <StackOverFlow />
+            <YellowCardContent text="The Ask Wizard (2022) has graduated" />
+          </InnerContent>
+        </YellowCardStyles>
+      </CardStyle>
+    </>
+  );
+};
+
+export default YCExample;
---
+++ client/src/components/Aside/YellowCard/YellowCardStyles.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+import { HiPencil } from "react-icons/hi";
+import { IoChatboxOutline, IoLogoStackoverflow } from "react-icons/io5";
+
+// 이거 구현할꺼면 a태그로 바꾸기
+export const YellowCardStyles = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: hsl(47, 87%, 94%);
+  border-left: calc(1px / 2) solid hsl(47, 65%, 84%);
+  border-right: calc(1px / 2) solid hsl(47, 65%, 84%);
+  border-bottom: calc(1px / 2) solid hsl(47, 65%, 84%);
+`;
+
+export const Pencil = styled(HiPencil)`
+  margin-left: 16px;
+  width: 16px;
+  height: 16px;
+`;
+
+export const Square = styled(IoChatboxOutline)`
+  margin-left: 16px;
+  color: hsl(206, 85%, 57.5%);
+  width: 16px;
+  height: 16px;
+`;
+
+export const StackOverFlow = styled(IoLogoStackoverflow)`
+  margin-left: 16px;
+  width: 16px;
+  height: 16px;
+`;
+
+const NumberIconStyle = styled.div`
+  display: inline-block;
+  font-size: 13px;
+  margin-left: 16px;
+  color: hsl(210, 8%, 45%);
+  width: 16px;
+  height: 16px;
+`;
+const NumberIcon = ({ text }) => {
+  return <NumberIconStyle>{text}</NumberIconStyle>;
+};
+export default NumberIcon;
