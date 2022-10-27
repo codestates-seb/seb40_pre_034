7f2ba8b3195dd6dd72d64179a28920ab3db6e912
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-10-28 03:15:24 +0900

    :sparkles: feat: Dto 구현

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/answer/dto/AnswerDto.java
@@ -0,0 +1,45 @@
+package seb40pre034.stackoverflowclone.answer.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+public class AnswerDto {
+
+    @AllArgsConstructor
+    @Getter
+    public static class Post {
+
+        @NotBlank(message = "답변 내용을 입력해 주세요.")
+        private String answer_content;
+        private List<String> imgUrls;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Patch {
+        private long answerId;
+
+        @NotBlank(message = "답변 내용을 입력해 주세요.")
+        private String answer_content;
+        private List<String> ImgUrls;
+
+        public void setAnswerId(long answerId) {
+            this.answerId = answerId;
+        }
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class Response {
+        private long answerId;
+        private String answer_content;
+        private List<String> imgUrls;
+        private LocalDateTime date;
+        private Integer vote;
+    }
+
+}
