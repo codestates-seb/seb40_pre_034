01a704c0881a61156350a04b2839b5cde06e5ba7
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-10-27 21:30:23 +0900

    :art: feat: Answer Entitiy 코드 수정

--- server/src/main/java/seb40pre034/stackoverflowclone/answer/entity/Answer.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/answer/entity/Answer.java
@@ -1,14 +1,17 @@
 package seb40pre034.stackoverflowclone.answer.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
 import org.w3c.dom.Text;
 import seb40pre034.stackoverflowclone.audit.Auditable;
 
 import javax.persistence.*;
 
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 @Entity
@@ -19,22 +22,23 @@ public class Answer extends Auditable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long answerId;
 
-    @Column(nullable = false)
+    @Column(columnDefinition = "TEXT", nullable = false)
     private String answer_content;
 
     @Column(nullable = false)
-    private int Answer_vote = 0;
+    @ColumnDefault("0")
+    private int Answer_vote;
 
 
 
 //  회원 ID
-    @ManyToOne
-    @JoinColumn(name = "MEMBER_ID")
-    private Long member_id;
+//    @ManyToOne
+//    @JoinColumn(name = "MEMBER_ID")
+//    private Long member_id;
 
 //  게시글 ID
-    @ManyToOne
-    @JoinColumn(name = "QUESTION_ID")
-    private Long question_id;
+//    @ManyToOne
+//    @JoinColumn(name = "QUESTION_ID")
+//    private Long question_id;
 
 }
