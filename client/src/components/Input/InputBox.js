682ec787f7ec7d05c7f2a7e12b646ff1a4ed8362
opc13579 <opc13579@gmail.com>
2022-10-29 02:24:36 +0900

    💄 design: input 컴포넌트 추가 수정

--- client/src/components/Input/InputBox.js
+++ client/src/components/Input/InputBox.js
@@ -1,29 +1,64 @@
 import styled from "styled-components";
 
+const InputBoxed = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  line-height: 15.7px;
+  font-size: 12px;
+  font-weight: 400;
+  color: rgb(106, 115, 124);
+  margin: 2px 0px;
+  padding: 0px 2px;
+  width: ${(props) => (props.width ? props.width : "268px")};
+  height: 32px;
+`;
+
+const Forgot = styled.div`
+  float: right;
+  width: ${(props) => (props.width ? props.width : "650px")};
+  font-size: 12px;
+  color: rgb(0, 116, 204);
+`;
+
 const InputContainer = styled.div`
-  background-color: none;
+  color: hsl(210, 8%, 5%);
+  padding: 0px 2px;
+  padding-top: 0px;
+  padding-right: 2px;
+  padding-bottom: 0px;
+  padding-left: 2px;
+  font-weight: 600;
 `;
 
 const Input = styled.input`
   width: ${(props) => (props.width ? props.width : "268px")};
-  padding: 8px 9px;
-
-  border: 1px solid hsl(210, 8%, 75%);
+  padding: 8px 10px;
+  margin-bottom: 10px;
+  height: 35px;
+  border: 1px solid rgb(186, 191, 196);
   border-radius: 3px;
-  background-color: hsl(0, 0%, 100%);
-  color: hsl(210, 8%, 5%);
-  font-size: 13px;
-
   &:focus {
+    border-color: #0a95ff;
     outline: none;
   }
 `;
 
 const InputBox = () => {
   return (
-    <InputContainer className="inputContainer">
-      <Input type="text" />
-    </InputContainer>
+    <>
+      <InputContainer className="inputContainer">
+        <div>Display name</div>
+        <Input type="text" />
+        <div>Email</div>
+        <Input type="text" />
+        <div>Password</div> <Forgot>Forgot password?</Forgot>
+        <Input type="text" />
+      </InputContainer>
+      <InputBoxed>
+        Passwords must contain at least eight characters, including at least 1 letter and 1 number.
+      </InputBoxed>
+    </>
   );
 };
 
