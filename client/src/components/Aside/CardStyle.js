86b0d80ca388820add1fc0811a4a0edc5dc8c7e5
EUNSEO814 <eunseo814@gmail.com>
2022-10-28 11:29:28 +0900

    💄 design : Aside Component 수정

--- client/src/components/Aside/CardStyle.js
+++ client/src/components/Aside/CardStyle.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const CardStyle = styled.div`
-  width: 300px;
+  width: ${(props) => (props.width ? props.width : "300px")};
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05);
   margin-bottom: 16px;
   border-radius: 3px;
