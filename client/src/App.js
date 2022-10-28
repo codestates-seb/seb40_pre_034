ef183e5d144484deddea17533ecb1499a3b8578d
opc13579 <opc13579@gmail.com>
2022-10-28 09:16:47 +0900

    💄 design: 앱 컴포넌트 추가

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
+import React from "react";
+import InputBox from "./components/Content/Input/InputBox";
+import { TagsInput } from "./components/Content/Input/TagsInput";
+import SearchBar from "./components/Content/Search/SearchBar";
+import SignupInfo from "./components/Content/SignupInfo/SignupInfo";
 import GlobalStyles from "./style/GlobalStyle";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <div>test</div>
+      <InputBox />
+      <SearchBar />
+      <TagsInput />
+      <SignupInfo />
     </>
   );
 }
