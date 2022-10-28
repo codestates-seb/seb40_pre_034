d3feaf6d4b3a1b14900bacec36d68be647a777a3
opc13579 <opc13579@gmail.com>
2022-10-28 23:25:36 +0900

    💄 design: InputBox 컴포넌트 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
+import SearchBar from "./components/Search/SearchBar";
 import SignupInfo from "./components/SignupInfo/SignupInfo";
 import GlobalStyles from "./style/GlobalStyle";
+import InputBox from "./components/Input/InputBox";
 
 function App() {
   return (
     <>
       <GlobalStyles />
+      <InputBox />
       <SignupInfo />
+      <SearchBar />
     </>
   );
 }
--- client/src/components/Input/InputBox.js
+++ client/src/components/Input/InputBox.js
@@ -1,45 +1,29 @@
 import styled from "styled-components";
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 60px;
-  margin-top: 7px;
-  input {
-    width: 100%;
-    height: 33px;
-    border: 1px solid #babfc4;
-    border-radius: 3px;
-    margin-top: 5px;
-    padding: 8px 9px;
-    ::placeholder {
-      color: red;
-      font-style: italic;
-    }
-  }
+const InputContainer = styled.div`
+  background-color: none;
 `;
 
-const Box = styled.div`
-  label {
-    padding: 0 2px;
-    font-size: 15px;
-    font-weight: 600;
-  }
-  span {
-    float: right;
-    font-size: 12px;
-    color: #0074cc;
+const Input = styled.input`
+  width: ${(props) => (props.width ? props.width : "268px")};
+  padding: 8px 9px;
+
+  border: 1px solid hsl(210, 8%, 75%);
+  border-radius: 3px;
+  background-color: hsl(0, 0%, 100%);
+  color: hsl(210, 8%, 5%);
+  font-size: 13px;
+
+  &:focus {
+    outline: none;
   }
 `;
 
 const InputBox = () => {
   return (
-    <Container>
-      <Box>
-        <label htmlFor="title">displayName, email, password</label>
-        <span></span>
-      </Box>
-    </Container>
+    <InputContainer className="inputContainer">
+      <Input type="text" />
+    </InputContainer>
   );
 };
 
