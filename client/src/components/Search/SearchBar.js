81249732298c0811d4f7fbdd93f901d6d5d05a8e
opc13579 <opc13579@gmail.com>
2022-10-28 21:33:11 +0900

    💄 design: SearchBar props 추가

---
+++ client/src/components/Search/SearchBar.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import { BiSearchAlt2 } from "react-icons/bi";
+
+const Container = styled.div`
+  display: flex;
+  justify-items: center;
+  align-items: center;
+  width: ${(props) => props.width || "50%"};
+  margin: 0 10px;
+  svg {
+    position: absolute;
+    margin-left: 10px;
+  }
+`;
+
+const SearchInput = styled.input`
+  border-radius: 3px;
+  border: 1px solid #babfc4;
+  background: #fff;
+  width: 100%;
+  height: 32px;
+  padding-left: 35px;
+  &::placeholder {
+    color: #838c95;
+  }
+`;
+
+const SearchBar = () => {
+  return (
+    <Container>
+      <SearchInput type="text" placeholder="Search..." />
+      <BiSearchAlt2 />
+    </Container>
+  );
+};
+export default SearchBar;
