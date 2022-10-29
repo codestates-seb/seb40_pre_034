49349f510ef9fd64e7af196173ae829a911f04f1
opc13579 <opc13579@gmail.com>
2022-10-29 11:08:37 +0900

    💄 design: InputBox 삭제 후 수정

--- client/src/components/Input/InputBox.js
+++ client/src/components/Input/InputBox.js
@@ -1,26 +1,5 @@
 import styled from "styled-components";
 
-const InputBoxed = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  line-height: 15.7px;
-  font-size: 12px;
-  font-weight: 400;
-  color: rgb(106, 115, 124);
-  margin: 2px 0px;
-  padding: 0px 2px;
-  width: ${(props) => (props.width ? props.width : "268px")};
-  height: 32px;
-`;
-
-const Forgot = styled.div`
-  float: right;
-  width: ${(props) => (props.width ? props.width : "650px")};
-  font-size: 12px;
-  color: rgb(0, 116, 204);
-`;
-
 const InputContainer = styled.div`
   color: hsl(210, 8%, 5%);
   padding: 0px 2px;
@@ -48,16 +27,8 @@ const InputBox = () => {
   return (
     <>
       <InputContainer className="inputContainer">
-        <div>Display name</div>
-        <Input type="text" />
-        <div>Email</div>
-        <Input type="text" />
-        <div>Password</div> <Forgot>Forgot password?</Forgot>
         <Input type="text" />
       </InputContainer>
-      <InputBoxed>
-        Passwords must contain at least eight characters, including at least 1 letter and 1 number.
-      </InputBoxed>
     </>
   );
 };
