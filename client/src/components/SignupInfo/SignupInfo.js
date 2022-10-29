193df0175fabda8b1e5f1d2690d2c71e13e763fe
opc13579 <opc13579@gmail.com>
2022-10-29 11:00:38 +0900

    💄design: SignupInfo 컴포넌트 추가 수정

--- client/src/components/SignupInfo/SignupInfo.js
+++ client/src/components/SignupInfo/SignupInfo.js
@@ -1,23 +1,10 @@
 import styled from "styled-components";
-import { RiShareBoxLine } from "react-icons/ri";
 
 const SignupSection = styled.div`
   display: flex;
   flex-direction: column;
 `;
 
-const BottomText = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  height: 78px;
-  font-size: 13px;
-  padding: 16px;
-  span {
-    color: #0074cc;
-  }
-`;
 const BottomDescription = styled.div`
   margin-top: 2rem;
   font-size: 13px;
@@ -35,13 +22,6 @@ const SignupInfo = () => {
         <span> terms of service, privacy policy</span> and
         <span> cookie policy</span>
       </BottomDescription>
-      <BottomText>
-        Already have an account? <span>Log in</span>
-        <div>
-          Are you an employer? Sign up on Talent
-          <RiShareBoxLine />
-        </div>
-      </BottomText>
     </SignupSection>
   );
 };
