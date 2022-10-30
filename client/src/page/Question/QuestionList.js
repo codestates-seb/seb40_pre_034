e8342c09e272141b130ffe54c4d8dbbaea22e9fa
KimYunhee <qnvnfls@naver.com>
2022-10-30 15:18:39 +0900

    💄 design: QuestionList 컴포넌트 레이아웃 수정

--- client/src/page/Question/QuestionList.js
+++ client/src/page/Question/QuestionList.js
@@ -1,13 +1,24 @@
 import styled from "styled-components";
 
 import BlueButton from "../../components/Button/BlueButton";
+import LightBlueButtonWithIcon from "../../components/Button/LightBlueButtonWithIcon";
+import SortedTabt from "../../components/SortedTab/SortedTab";
 import QuestionsCount from "../../components/QuestionsCount/QuestionsCount";
 import QuestionElement from "../../components/QuestionElement/QuestionElement";
+import YellowCard from "../../components/SideCard/YellowCard/YCExample";
+import WhiteCard from "../../components/SideCard/WhiteCard";
 
 import dummyData from "./dummyData";
 
 const Container = styled.div`
+  display: flex;
   padding: 20px;
+`;
+
+const SideDescription = styled.div``;
+
+const QuestionContainer = styled.div`
+  padding: 0 20px;
 `;
 
 const QuestionHeader = styled.div`
@@ -22,45 +33,66 @@ const QuestionHeader = styled.div`
 const QuestionOption = styled.div`
   display: flex;
   justify-content: space-between;
-  margin-bottom: 20px;
+  align-items: center;
+  margin-bottom: 15px;
+  padding-top: 30px;
 `;
 
-const QuestionSort = styled.div``;
+const QuestionSort = styled.div`
+  margin-right: 20px;
+`;
 
 const Questions = styled.ul``;
+
+const ButtonContainer = styled.div`
+  display: flex;
+`;
 
 const QuestionList = () => {
   return (
     <Container>
-      <QuestionHeader>
-        <h1>All Questions</h1>
-        <BlueButton text="Ask Question" width="100px" />
-      </QuestionHeader>
+      <QuestionContainer>
+        <QuestionHeader>
+          <h1>All Questions</h1>
+          <BlueButton text="Ask Question" />
+        </QuestionHeader>
 
-      <QuestionOption>
-        {/* count 서버에서 받아와야함 */}
-        <QuestionsCount count={10} text="questions" />
-        <QuestionSort>{/* SortedTab 자리 */}</QuestionSort>
-      </QuestionOption>
+        <QuestionOption>
+          {/* count 서버에서 받아와야함 */}
+          <QuestionsCount count={10} text="questions" />
+          <ButtonContainer>
+            <QuestionSort>
+              <SortedTabt text="Newest" />
+              <SortedTabt text="Popular" />
+            </QuestionSort>
+            <LightBlueButtonWithIcon isFilter text="Filter" />
+          </ButtonContainer>
+        </QuestionOption>
 
-      <Questions>
-        {dummyData.map((question, idx) => {
-          return (
-            <li key={idx}>
-              <QuestionElement
-                voteCnt={question.vote}
-                answersCnt={question.answers}
-                viewsCnt={question.views}
-                title={question.title}
-                content={question.content}
-                tags={question.tags}
-                nickname={question.nickname}
-                createdAt={question.createdAt}
-              />
-            </li>
-          );
-        })}
-      </Questions>
+        <Questions>
+          {dummyData.map((question, idx) => {
+            return (
+              <li key={idx}>
+                <QuestionElement
+                  voteCnt={question.vote}
+                  answersCnt={question.answers}
+                  viewsCnt={question.views}
+                  title={question.title}
+                  content={question.content}
+                  tags={question.tags}
+                  nickname={question.nickname}
+                  createdAt={question.createdAt}
+                />
+              </li>
+            );
+          })}
+        </Questions>
+      </QuestionContainer>
+      <SideDescription>
+        <YellowCard />
+        <WhiteCard text="Custom Filters" />
+        <WhiteCard text="Watched Tags" />
+      </SideDescription>
     </Container>
   );
 };
