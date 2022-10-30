64e25304a28a15999c72393b14a337f6a39fb794
opc13579 <opc13579@gmail.com>
2022-10-31 02:54:23 +0900

    🐛 fix: 병합 오류 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,9 @@
 import GlobalStyles from "./style/GlobalStyle";
-import TagPage from "./page/TagPage/TagPage";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <TagPage />
     </>
   );
 }
--- client/src/components/SortedTab/SortedTab.js
+++ client/src/components/SortedTab/SortedTab.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-const SortedTabStyle = styled.button`
+const SortedTabBox = styled.button`
   border-radius: 3px;
   width: ${(props) => (props.width ? props.width : "68.44px")};
   height: ${(props) => (props.height ? props.height : "33px")};
@@ -30,7 +30,7 @@ const SortedTabStyle = styled.button`
 `;
 
 const SortedTab = ({ text }) => {
-  return <SortedTabStyle>{text}</SortedTabStyle>;
+  return <SortedTabBox>{text}</SortedTabBox>;
 };
 
 export default SortedTab;
