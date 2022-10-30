2b10f83386cfc075b0349ff5cfd6fd0378fc4741
EUNSEO814 <eunseo814@gmail.com>
2022-10-30 20:09:29 +0900

    💄 design : 체크박스, 카드스타일, 디자인 변경

--- client/src/components/SideCard/CardStyle.js
+++ client/src/components/SideCard/CardStyle.js
@@ -4,5 +4,5 @@ export const CardStyle = styled.div`
   width: ${(props) => (props.width ? props.width : "300px")};
   box-shadow: 0 1px 2px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.05), 0 2px 8px hsla(0, 0%, 0%, 0.05);
   margin-bottom: 16px;
-  border-radius: 3px;
+  border-radius: 6px;
 `;
