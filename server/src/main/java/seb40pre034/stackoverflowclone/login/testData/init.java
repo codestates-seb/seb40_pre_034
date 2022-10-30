21d10b7f5798f27e3a9180cd122856225f6b2db8
ClimberCode <kcjckswnd@gamil.com>
2022-10-30 23:00:59 +0900

    :spackles: chore : testData 추가

--- server/src/main/java/seb40pre034/stackoverflowclone/login/testData/init.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/login/testData/init.java
@@ -1,2 +1,24 @@
-package seb40pre034.stackoverflowclone.login.testData;public class init {
+package seb40pre034.stackoverflowclone.login.testData;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import seb40pre034.stackoverflowclone.member.entity.Member;
+import seb40pre034.stackoverflowclone.member.repository.MemberRepository;
+import seb40pre034.stackoverflowclone.member.service.MemberService;
+
+import javax.annotation.PostConstruct;
+
+@Component
+@RequiredArgsConstructor
+public class init {
+    private final MemberRepository repository;
+    @PostConstruct
+    public void memberInit() {
+        Member member = new Member("test@test.com", "test", "test1234!@#$");
+        member.setMemberId(1L);
+        member.setMemberStatus(Member.MemberStatus.MEMBER_ACTIVE);
+
+        repository.save(member);
+    }
 }
