7a79c1df2cfa897048fe761f5206cd326126b9e9
EUNSEO814 <eunseo814@gmail.com>
2022-10-31 17:43:39 +0900

    ✨ feat :  json-server 연결

--- client/src/page/Login/LoginInput.js
+++ client/src/page/Login/LoginInput.js
@@ -1,10 +1,11 @@
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
 import BlueButton from "../../components/Button/BlueButton";
 import { CardStyle } from "../../components/SideCard/CardStyle";
-// import InputBox from "../../components/Input/InputBox";
-// import { MdError } from "react-icons/md";
+import axios from "axios";
 
 const InputArea = styled.div`
+  width: 316px;
   background-color: hsl(0, 0%, 100%);
   padding: 24px;
   margin-bottom: 24px;
@@ -15,7 +16,7 @@ const InputArea = styled.div`
   border-radius: 7px;
 `;
 
-const InputTiTle = styled.div`
+const InputForm = styled.form`
   margin-top: 6px;
   margin-bottom: 8px;
   display: flex;
@@ -54,46 +55,81 @@ const Input = styled.input`
   }
 `;
 
-const FailInput = styled.input`
-  width: ${(props) => (props.width ? props.width : "268px")};
-  padding: 8px 10px;
-  margin-bottom: 10px;
-  height: 35px;
-  border-radius: 3px;
-  border: 1px solid rgb(222, 79, 84);
-  &:focus {
-    border-color: red;
-    outline: 4px solid rgb(249, 210, 211);
-  }
-`;
-
 const ErrorMSG = styled.div`
   color: rgb(222, 79, 84);
+  margin-bottom: 6px;
 `;
 
-// const isCorrectID = () => {};
-// const isCorrectPW = () => {};
-// const isMoreThan8 = () => {};
-
 const LoginInput = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    await new Promise((r) => setTimeout(r, 1000));
+
+    const login = {
+      email: data.email,
+      password: data.password,
+    };
+    console.log(login);
+
+    axios
+      .post("http://localhost:4000/login", login)
+      .then(
+        (res) => console.log(res.data),
+        // {
+        //   if (res.status === 201) {
+        //     console.log("페이지 이동");
+        //   }
+        // }
+      )
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <CardStyle width="100%">
+    <CardStyle width="316px">
       <InputArea>
-        <InputTiTle>
-          <InputTxt>Email</InputTxt>
-          <Input />
-          <FailInput type="text" placeholder="Example@example.com"></FailInput>
-          <ErrorMSG>The email is not a valid email address.</ErrorMSG>
-          <ErrorMSG>The email or password is incorrect.</ErrorMSG>
-        </InputTiTle>
-        <InputTiTle>
+        <InputForm onSubmit={handleSubmit(onSubmit)}>
+          <InputTxt htmlFor="email">Email</InputTxt>
+          <Input
+            id="email"
+            type="text"
+            placeholder="Example@email.com"
+            {...register("email", {
+              required: "이메일은 필수 입력입니다.",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "이메일 형식에 맞지 않습니다.",
+              },
+            })}
+          />
+          {errors.email && <ErrorMSG role="alert">{errors.email.message}</ErrorMSG>}
           <PwArea>
-            <InputTxt>Password</InputTxt>
+            <InputTxt htmlFor="password">Password</InputTxt>
             <Anchor2 href="/">forgot password?</Anchor2>
           </PwArea>
-          <Input />
-        </InputTiTle>
-        <BlueButton text="Log in" width="268px" />
+          <Input
+            id="password"
+            type="password"
+            placeholder="********"
+            {...register("password", {
+              required: "비밀번호는 필수 입력입니다.",
+              minLength: {
+                value: 8,
+                message: "8자리 이상 비밀번호를 사용하세요.",
+              },
+              pattern: {
+                value: /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/,
+                message: "영문, 숫자, 특수기호 조합으로 8-20자리 이상 입력해주세요.",
+              },
+            })}
+          />
+          {errors.password && <ErrorMSG role="alert">{errors.password.message}</ErrorMSG>}
+          <BlueButton text="Log in" type="submit" disabled={isSubmitting} />
+        </InputForm>
       </InputArea>
     </CardStyle>
   );
