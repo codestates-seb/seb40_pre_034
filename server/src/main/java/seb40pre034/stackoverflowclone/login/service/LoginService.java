f6711332f2813ac2422eb1b172fbd4322f2e6e18
ClimberCode <kcjckswnd@gamil.com>
2022-10-31 17:08:09 +0900

    :spakles: feat: loginService 구현

--- server/src/main/java/seb40pre034/stackoverflowclone/login/service/LoginService.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/login/service/LoginService.java
@@ -1,2 +1,23 @@
-package seb40pre034.stackoverflowclone.login.service;public class LoginService {
+package seb40pre034.stackoverflowclone.login.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import seb40pre034.stackoverflowclone.member.entity.Member;
+import seb40pre034.stackoverflowclone.member.repository.MemberRepository;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+    private final MemberRepository repository;
+
+    public Member login(String email, String password) {
+
+        return repository.findByEmail(email)
+                .filter(m -> m.getPassword().equals(password))
+                .orElse(null);
+    }
 }
