110973de6cacce9a445900425cc38c48e15850b0
KimYunhee <qnvnfls@naver.com>
2022-11-01 14:07:37 +0900

    ⚡️ feat: 닉네임, 이메일, 비밀번호 유효성 검사 추가

--- client/src/page/Signup/Signup.js
+++ client/src/page/Signup/Signup.js
@@ -4,8 +4,10 @@ import axios from "axios";
 
 import SignupInfo from "../../components/SignupInfo/SignupInfo";
 import SignupIntro from "../../components/SignupInfo/SignupIntro";
-import InputBox from "../../components/Input/InputBox";
+import SignupInfoAdd from "../../components/SignupInfo/SignupInfoAdd";
+import SignupInput from "./SignupInput";
 import BlueButton from "../../components/Button/BlueButton";
+import SocialBtn from "../Login/SocialBtn";
 
 const Container = styled.div`
   background-color: #f1f2f3;
@@ -30,20 +32,11 @@ const SignupForm = styled.form`
     rgba(0, 0, 0, 0.1) 0px 1px 4px 0px;
   border-radius: 7px;
   padding: 20px;
-  margin-bottom: 10px;
-  max-width: 330px;
+  margin-bottom: 30px;
+  max-width: 316px;
 
   p {
     color: #6a737c;
-    margin-bottom: 10px;
-  }
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  & > div {
-    font-weight: 600;
     margin-bottom: 10px;
   }
 `;
@@ -66,25 +59,57 @@ const Signup = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nameErrorMsg, setNameErrorMsg] = useState("");
+  const [emailErrorMsg, setEmailErrorMsg] = useState("");
+  const [pwdErrorMsg, setPwdErrorMsg] = useState("");
 
-  // const isEmailValid = () => {
-  //   const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  const isValid = (type, value) => {
+    const pattern = {
+      nickname: /[A-Za-z0-9]{4,12}/,
+      email: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])+\.[a-zA-Z]{2,3}$/i,
+      password: /[(a-zA-Z0-9)`~!@#$%^&-_=\\+]{8,12}/,
+    };
 
-  //   return pattern.test(email);
-  // };
+    if (type === "nickname") {
+      return pattern.nickname.test(value);
+    } else if (type === "email") {
+      return pattern.email.test(value);
+    } else {
+      return pattern.password.test(value);
+    }
+  };
+
+  const checkInputVal = () => {
+    if (name.length <= 0) {
+      setNameErrorMsg("Name cannot be empty");
+    } else if (!isValid("nickname", name)) {
+      setNameErrorMsg(`${name} is not a valid name`);
+    }
+
+    if (email.length <= 0) {
+      setEmailErrorMsg("Email cannot be empty");
+    } else if (!isValid("email", email)) {
+      setEmailErrorMsg(`${email} is not a valid email address`);
+    }
+
+    if (password.length <= 0) {
+      setPwdErrorMsg("Password cannot be empty");
+    } else if (!isValid("password", password)) {
+      setPwdErrorMsg(`${password} is not a valid password`);
+    }
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    // console.log(isEmailValid(email));
-    const user = {
-      nickname: name,
-      email: email,
-      password: password,
-    };
+    checkInputVal();
 
     axios
-      .post("http://localhost:4001/users", user)
+      .post("/members/signup", {
+        nickName: name,
+        email: email,
+        password: password,
+      })
       .then((res) => console.log(res.status))
       .catch((error) => console.log(error));
   };
@@ -93,21 +118,17 @@ const Signup = () => {
     <Container>
       <SignupIntro />
       <SignupContainer>
-        {/* 소셜로그인 버튼자리 */}
+        <SocialBtn />
         <SignupForm>
-          <InputContainer>
-            <div>Display name</div>
-            <InputBox value={name} handleValue={setName} />
-          </InputContainer>
-          <InputContainer>
-            <div>Email</div>
-            {/* 유효성 검사 */}
-            <InputBox value={email} handleValue={setEmail} />
-          </InputContainer>
-          <InputContainer>
-            <div>Password</div>
-            <InputBox value={password} handleValue={setPassword} />
-          </InputContainer>
+          <SignupInput title="Display name" value={name} handleValue={setName} errorMsg={nameErrorMsg} />
+          <SignupInput title="Email" value={email} handleValue={setEmail} errorMsg={emailErrorMsg} />
+          <SignupInput
+            title="Password"
+            type="password"
+            value={password}
+            handleValue={setPassword}
+            errorMsg={pwdErrorMsg}
+          />
           <p>Passwords must contain at least eight characters, including at least 1 letter and 1 number.</p>
           <CheckboxContainer>
             <input type="checkbox" />
@@ -119,7 +140,7 @@ const Signup = () => {
           <BlueButton text="Sign up" width="268px" handleSubmit={handleSubmit} />
           <SignupInfo />
         </SignupForm>
-        {/* SignupInfoAdd 자리 */}
+        <SignupInfoAdd />
       </SignupContainer>
     </Container>
   );
