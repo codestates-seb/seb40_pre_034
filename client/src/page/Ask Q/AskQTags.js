2446260e77322315d561c28dd206255e3b9afe1c
opc13579 <opc13579@gmail.com>
2022-11-01 20:05:37 +0900

    💄 design: AskQTags 추가

---
+++ client/src/page/Ask Q/AskQTags.js
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+import { Tag } from "./AskQuestion";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid #babfc4;
+  border-radius: 3px;
+  > ul {
+    display: flex;
+    align-items: center;
+    gap: 3px;
+  }
+  > input {
+    flex-grow: 1;
+    border: none;
+    font-size: 14px;
+    padding: 7px;
+    :focus {
+      outline: transparent;
+    }
+  }
+  &:focus-within {
+    outline: 1px solid #59a4de;
+  }
+`;
+
+const AskQTags = ({ tags, setTags }) => {
+  const removeTags = (indexToRemove) => {
+    setTags(tags.filter((_, index) => index !== indexToRemove));
+  };
+
+  const addTags = (event) => {
+    const filtered = tags.filter((el) => el === event.target.value);
+    if (event.target.value !== "" && filtered.length === 0) {
+      setTags([...tags, event.target.value]);
+      event.target.value = "";
+    }
+  };
+
+  return (
+    <Container>
+      <ul id="tags">
+        {tags.map((el, index) => (
+          <li key={index} className="tag">
+            <Tag>
+              {el}
+              <button className="tag-close-icon" onClick={() => removeTags(index)}></button>
+              {/* <span className="tag-close-icon" onClick={() => removeTags(index)}> */}
+              &times;
+              {/* </span> */}
+              {/* span 써야 제대로 나오는데 에러 뜸 */}
+            </Tag>
+          </li>
+        ))}
+      </ul>
+      <input
+        className="tag-input"
+        type="text"
+        id="tag"
+        onKeyUp={(event) => (event.key === "Enter" || event.key === " " ? addTags(event) : null)}
+        placeholder="e.g. (ajax objective-c r)"
+      />
+    </Container>
+  );
+};
+
+export default AskQTags;
