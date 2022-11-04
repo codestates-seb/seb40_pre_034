a9c5dd7af02327ed1bb7c5338dfd8ae6f8c22f78
ClimberCode <kcjckswnd@gamil.com>
2022-11-05 01:57:20 +0900

    :art: chore : cors HTTP 요청 메서드 PATCH 추가

--- server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/CorsSecurityFilter.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/CorsSecurityFilter.java
@@ -14,7 +14,7 @@ public class CorsSecurityFilter implements Filter {
 
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-        response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,PATCH,OPTIONS,DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
 
         chain.doFilter(req, response);
