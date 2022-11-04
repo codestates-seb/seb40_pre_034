26b9f511e108f9b90ea2278e29b406b2fe27c15f
ClimberCode <kcjckswnd@gamil.com>
2022-11-04 13:58:56 +0900

    :sparkles: feat : 회원 인증을 위한 JwtAuthenticationFilter 구현

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,73 @@
+package seb40pre034.stackoverflowclone.config.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import seb40pre034.stackoverflowclone.config.auth.JwtTokenizer;
+import seb40pre034.stackoverflowclone.member.dto.LoginDto;
+import seb40pre034.stackoverflowclone.member.entity.Member;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final JwtTokenizer tokenizer;
+    private final AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(JwtTokenizer tokenizer, AuthenticationManager authenticationManager) {
+        this.tokenizer = tokenizer;
+        this.authenticationManager = authenticationManager;
+    }
+
+    @SneakyThrows
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        LoginDto loginDto = objectMapper.readValue(request.getInputStream(), LoginDto.class);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+        return authenticationManager.authenticate(authenticationToken);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, Authentication authResult) {
+        Member member = (Member)authResult.getPrincipal();
+
+        String accessToken = delegateAccessToken(member);
+        String refreshToken = delegateRefreshToken(member);
+
+        response.setHeader("Authorization", "Bearer" + accessToken);
+        response.setHeader("Refresh", refreshToken);
+    }
+
+    private String delegateAccessToken(Member member) {
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("username", member.getEmail());
+        claims.put("member-id", member.getMemberId());
+
+        String subject = member.getEmail();
+        Date expiration = tokenizer.getExpiration(tokenizer.getAccessTokenExpirationMinutes());
+
+        String base64EncodedSecretKey = tokenizer.encodeBase64SecretKey(tokenizer.getSecretKey());
+
+        String accessToken = tokenizer.generateAccessToken(claims, subject, expiration, base64EncodedSecretKey);
+
+        return accessToken;
+    }
+
+    private String delegateRefreshToken(Member member) {
+        String subject = member.getEmail();
+        Date expiration = tokenizer.getExpiration(tokenizer.getRefreshTokenExpirationMinutes());
+        String base64EncodedSecretKey = tokenizer.encodeBase64SecretKey(tokenizer.getSecretKey());
+        String refreshToken = tokenizer.generateRefreshToken(subject, expiration, base64EncodedSecretKey);
+
+        return refreshToken;
+    }
+}
