ed81417c8932f8826cb6d394db65466ef9861815
homebird9 <107456299+homebird9@users.noreply.github.com>
2022-11-04 15:42:30 +0900

    test

--- server/src/main/java/seb40pre034/stackoverflowclone/exception/ExceptionCode.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/exception/ExceptionCode.java
@@ -8,9 +8,8 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409, "동일한 이메일이 존재합니다."),
     QUESTION_NOT_FOUND(404, "게시글을 찾을 수 없습니다."),
     ANSWER_NOT_FOUND(404, "Answer not found"),
-    COMMENT_NOT_FOUND(404,"Comment not found");
+    COMMENT_NOT_FOUND(404,"Comment not found"),
     TAG_NOT_FOUND(404, "해당 태그를 찾을 수 없습니다.");
-    ;
 
     @Getter
     private int code;
