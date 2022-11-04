e6bd1c6844730448c8fdec4a055b18f042a4a333
ClimberCode <kcjckswnd@gamil.com>
2022-11-04 14:09:06 +0900

    :art: chore : 더미데이터 패스워드 암호화

--- server/src/main/java/seb40pre034/stackoverflowclone/login/testData/init.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/login/testData/init.java
@@ -2,6 +2,7 @@ package seb40pre034.stackoverflowclone.login.testData;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import seb40pre034.stackoverflowclone.member.entity.Member;
 import seb40pre034.stackoverflowclone.member.repository.MemberRepository;
@@ -13,9 +14,10 @@ import javax.annotation.PostConstruct;
 @RequiredArgsConstructor
 public class init {
     private final MemberRepository repository;
+    private final PasswordEncoder passwordEncoder;
     @PostConstruct
     public void memberInit() {
-        Member member = new Member("test@test.com", "test", "test1234!@#$");
+        Member member = new Member("test@test.com", "test", passwordEncoder.encode("test1234!@#$"));
         member.setMemberId(1L);
         member.setMemberStatus(Member.MemberStatus.MEMBER_ACTIVE);
 
