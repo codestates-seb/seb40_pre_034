c6bbc7528a00dd0d0aeac312e2e8895e8a74b888
ClimberCode <kcjckswnd@gamil.com>
2022-11-04 23:15:03 +0900

    :sparkles: feat : CORS Filter 구현

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/CorsFilter.java
@@ -0,0 +1,26 @@
+package seb40pre034.stackoverflowclone.config.auth.filter;
+
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
+        response.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+
+        chain.doFilter(req, response);
+    }
+
+    public void init(FilterConfig arg0) throws ServletException {}
+
+    public void destroy() {}
+}
