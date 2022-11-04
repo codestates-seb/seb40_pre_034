fffa6883447029eb0429e049ed2f9d4f2c59a046
oasis791 <khwi791@naver.com>
2022-11-04 11:24:03 +0900

    :sparkles: feat: Exception Code 추가

--- server/src/main/java/seb40pre034/stackoverflowclone/exception/ExceptionCode.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/exception/ExceptionCode.java
@@ -5,8 +5,10 @@ import lombok.Getter;
 public enum ExceptionCode {
     MEMBER_NOT_FOUND(404, "Member not found"),
     MEMBER_EXISTS(409, "Member exists"),
-    QUESTION_NOT_FOUND(404, "Question not found"),
-    ANSWER_NOT_FOUND(404, "Question not found");
+    QUESTION_NOT_FOUND(404, "게시글을 찾을 수 없습니다."),
+    ANSWER_NOT_FOUND(404, "Question not found"),
+
+    TAG_NOT_FOUND(404, "해당 태그를 찾을 수 없습니다.");
 
     @Getter
     private int code;
