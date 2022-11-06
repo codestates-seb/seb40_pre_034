3fa15a0ec1e66e1c0712be45dc765c68e4d574e9
oasis791 <khwi791@naver.com>
2022-11-07 00:34:50 +0900

    test

--- server/src/main/java/seb40pre034/stackoverflowclone/member/controller/MemberController.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/member/controller/MemberController.java
@@ -20,6 +20,7 @@ import java.util.List;
 @RequestMapping("/members")
 @Validated
 @Slf4j
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class MemberController {
     private final MemberService memberService;
     private final MemberMapper mapper;
