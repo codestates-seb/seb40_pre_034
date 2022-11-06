885571e8c0eba3a48e80a681bb9c4b4792421b61
ClimberCode <kcjckswnd@gamil.com>
2022-11-06 22:57:15 +0900

    hidden

--- server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/CorsSecurityFilter.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/CorsSecurityFilter.java
@@ -6,15 +6,15 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@Component
+//@Component
 public class CorsSecurityFilter implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 
         HttpServletResponse response = (HttpServletResponse) res;
 
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept");
-        response.setHeader("Access-Control-Allow-Methods", "POST,GET,PATCH,OPTIONS,DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
 
         chain.doFilter(req, response);
