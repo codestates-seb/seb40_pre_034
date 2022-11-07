de89f5e0c8792cdd740892c5235b7a60a1ccb0d2
oasis791 <khwi791@naver.com>
2022-11-07 09:35:05 +0900

    test

---
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/WebConfig.java
@@ -0,0 +1,15 @@
+package seb40pre034.stackoverflowclone.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
