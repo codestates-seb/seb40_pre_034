4a2a2b754fd3f0be5429f3b3b2b9d61a05e902ee
ClimberCode <kcjckswnd@gamil.com>
2022-11-07 10:08:30 +0900

    memberId

--- server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/JwtAuthenticationFilter.java
@@ -51,6 +51,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         Map<String, Object> claims = new HashMap<>();
         claims.put("username", member.getEmail());
         claims.put("roles", member.getRoles());
+        claims.put("memberId", member.getMemberId());
 
         String subject = member.getEmail();
         Date expiration = tokenizer.getExpiration(tokenizer.getAccessTokenExpirationMinutes());
