97f29e3968d57ab4d54666d2f45de6b01ae3653e
oasis791 <khwi791@naver.com>
2022-11-07 10:48:37 +0900

    test

--- server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/JwtAuthenticationFilter.java
+++ server/src/main/java/seb40pre034/stackoverflowclone/config/auth/filter/JwtAuthenticationFilter.java
@@ -43,7 +43,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String accessToken = delegateAccessToken(member);
         String refreshToken = delegateRefreshToken(member);
 
-        response.setHeader("Authorization", "Bearer" + accessToken);
+        response.setHeader("Authorization", "Bearer"+" "+ accessToken);
         response.setHeader("Refresh", refreshToken);
     }
 
